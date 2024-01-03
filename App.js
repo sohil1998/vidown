f2825a60f42ccda838529cd32416d5b5b0fa74c2
sohilshaikh1998 <sohilshaikh.aobs@gmail.com>
2024-01-03 13:44:16 +0530

    remove unwanted library

--- App.js
+++ App.js
@@ -8,50 +8,15 @@ import {
   Alert,
   Linking,
 } from "react-native";
-import { WebView } from "react-native-webview";
 import * as FileSystem from "expo-file-system";
 import * as MediaLibrary from "expo-media-library";
 import * as Sharing from "expo-sharing";
 import { Animation, Animation1 } from "./animation";
-import ReceiveSharingIntent from "react-native-receive-sharing-intent";
 
 const VideoScreen = () => {
-  const [textInput, setTextInput] = useState(
-    ""
-    // "https://www.instagram.com/p/Cud1vupN1l-/?utm_source=ig_web_copy_link"
-    //"https://www.instagram.com/p/CA5CsiZJMEJ/?utm_source=ig_web_copy_link"
-  );
-  // const [textInput1, setTextInput1] = useState("");
+  const [textInput, setTextInput] = useState("");
   const [progress, setProgress] = useState(0);
   const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // Example: Assume this is called when the app is opened with a deep link
-    handleDeepLink();
-  }, []);
-
-  const handleDeepLink = (url) => {
-    ReceiveSharingIntent.getReceivedFiles(
-      (files) => {
-        console.log(files);
-        // files returns as JSON Array example
-        //[{ filePath: null, text: null, weblink: null, mimeType: null, contentUri: null, fileName: null, extension: null }]
-      },
-      (error) => {
-        console.log(error);
-      },
-      "ShareMedia" // share url protocol (must be unique to your app, suggest using your apple bundle id)
-    );
-
-    // To clear Intents
-    ReceiveSharingIntent.clearReceivedFiles();
-  };
-
-  const downloadProgressCallback = (progressEvent) => {
-    const progress =
-      progressEvent.totalBytesWritten / progressEvent.totalBytesExpectedToWrite;
-    setProgress(progress);
-  };
 
   function extractYouTubeVideoId(url) {
     const regex =
@@ -60,15 +25,13 @@ const VideoScreen = () => {
     return match ? match[1] : null;
   }
 
-  // const API_URL = `http://192.168.0.115:3000/download?url=https://www.youtube.com/watch?v=`;
-  // const API_URL = `http://192.168.0.115:3000/`;
   const API_URL = `https://vidown.onrender.com/`;
 
   const handleDownload1 = async (url) => {
     const youtubeURL = await url;
     const { status } = await MediaLibrary.requestPermissionsAsync();
     try {
-      const downloadResult = await FileSystem.createDownloadResumable(
+      const downloadResult = FileSystem.createDownloadResumable(
         youtubeURL,
         FileSystem.documentDirectory +
           `video${Math.floor(Math.random() * (100000 - 0 + 1)) + 0}.mp4`
@@ -89,12 +52,9 @@ const VideoScreen = () => {
 
   const handleDownload = async () => {
     const videoId = await extractYouTubeVideoId(textInput);
-    let url = await `${API_URL}download?url=${videoId}`;
+    let url = `${API_URL}download?url=${videoId}`;
 
     console.log(url);
-
-    // let url =
-    //   "http://192.168.0.115:3000/download?url=https://www.youtube.com/watch?v=YT8rY_o5VhY";
 
     try {
       setLoading(true);
@@ -116,13 +76,9 @@ const VideoScreen = () => {
 
   const handleShare = async (videoUri) => {
     try {
-      // Check if the device supports sharing
       if (!(await Sharing.isAvailableAsync())) {
-        // Sharing is not available on this device
         return;
       }
-
-      // Share the video with the share dialog
       await Sharing.shareAsync(videoUri);
     } catch (error) {
       console.error("Error sharing video:", error);
@@ -133,8 +89,8 @@ const VideoScreen = () => {
   const handleDownloadIG = async () => {
     setLoading(true);
 
-    const videoId = await textInput;
-    let url = await `${API_URL}downloadIG?url=${videoId}`;
+    const videoId = textInput;
+    let url = `${API_URL}downloadIG?url=${videoId}`;
     console.log(videoId);
     fetch(url)
       .then((response) => response.json())
@@ -162,32 +118,6 @@ const VideoScreen = () => {
           setTextInput(e);
         }}
       />
-      {/* <TextInput
-        style={styles.textInput}
-        value={textInput1}
-        placeholder="Enter video URL"
-        onChangeText={(e) => {
-          console.log(e);
-        }}
-      /> */}
-      {/* <View style={styles.video}> */}
-      {/* <WebView
-          source={{
-            // uri: `https://www.youtube.com/embed/${extractYouTubeVideoId(
-            //   textInput
-            // )}`,
-            uri: `https://www.instagram.com/embed/${extractYouTubeVideoId(
-              textInput
-            )}`,
-          }}
-          style={{ flex: 1 }}
-          allowsFullscreenVideo={true}
-          allowsInlineMediaPlayback={true}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-        /> */}
-      {/* <Text></Text> */}
-      {/* </View> */}
 
       {textInput && textInput.includes("https") && (
         <TouchableOpacity
@@ -280,10 +210,3 @@ const styles = StyleSheet.create({
 });
 
 export default VideoScreen;
-
-const youtubeVideoUrl =
-  //"https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
-  "https://www.youtube.com/watch?v=YT8rY_o5VhY";
-("https://youtube.com/shorts/vcjpRaKLA04?feature=share");
-("https://youtu.be/vcjpRaKLA04");
-("https://www.youtube.com/shorts/vcjpRaKLA04");
